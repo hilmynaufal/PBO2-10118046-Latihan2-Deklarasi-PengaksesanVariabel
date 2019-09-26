6443e3efae6015d57e34c7388d4f8f5f6b8b853f
ASUS <ASUS@DESKTOP-6TMOBF9>
2019-09-26 18:51:39 +0700

    commit

---
+++ src/pbo2/pert1/pkg10118046/latihan2/deklarasi/pengaksesanvariabel/PBO2PERT110118046Latihan2DeklarasiPengaksesanVariabel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pert1.pkg10118046.latihan2.deklarasi.pengaksesanvariabel;
+
+/**
+ *
+ * @author 
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program : mendeklarasikan variabel dan menampilkan nya
+ */
+public class PBO2PERT110118046Latihan2DeklarasiPengaksesanVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        System.out.println();
+        System.out.println("Isi variabel nilai: " + nilaiInt);
+        System.out.println("Isi variable PHI: " + PHI);
+        System.out.println("Isi variabel logika: " + nilaiLogika);
+        System.out.println("Isi variabel karakter: " + nilaiKarakter);
+
+    }
+    
+}
